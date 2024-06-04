2b55313559efe74134986b54584bd1e2ea1cb74d
AlvaroGaribay1 <garciagaribayalvaro@gmail.com>
2024-06-03 20:03:35 -0600

    hola

---
+++ book-library/src/main/java/com/springproject/book_library/user/Token.java
@@ -0,0 +1,33 @@
+package com.springproject.book_library.user;
+
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Token{
+
+    @Id
+    @GeneratedValue
+    private Integer id;
+
+    private String token;
+    private LocalDateTime createdAt;
+    private LocalDateTime expiresAt;
+    private LocalDateTime validatedAt;
+
+    @ManyToOne()
+    @JoinColumn(name = "userid", nullable = false)
+    private User user;
+
+
+
+}
