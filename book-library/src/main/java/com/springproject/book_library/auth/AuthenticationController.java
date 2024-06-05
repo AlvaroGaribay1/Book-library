75ce283c75df5a2d3b50458318b29d1c9011031f
AlvaroGaribay1 <garciagaribayalvaro@gmail.com>
2024-06-04 22:45:48 -0600

    Auth creation and view for mail confirmation

---
+++ book-library/src/main/java/com/springproject/book_library/auth/AuthenticationController.java
@@ -0,0 +1,35 @@
+package com.springproject.book_library.auth;
+
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.mail.MessagingException;
+import jakarta.servlet.Registration;
+import jakarta.servlet.http.PushBuilder;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import lombok.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("auth")
+@RequiredArgsConstructor
+@Tag(name = "Authentication")
+public class AuthenticationController {
+
+    private final AuthenticationService service = null;
+
+    @PostMapping("/register")
+    @ResponseStatus(HttpStatus.ACCEPTED)
+    public ResponseEntity<?> register(
+            @RequestBody @Valid RegistrationRequest request
+    ) throws MessagingException {
+        service.register(request);
+        return ResponseEntity.accepted().build();
+    }
+
+
+
+
+
+}
